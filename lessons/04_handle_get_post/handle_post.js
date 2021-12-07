3692bf8b2641256ce6ff2403d94c7769f7df7f0e
Ankush Kunwar <ank.knr@gmail.com>
2021-12-07 17:43:33 +0545

    handle post request

---
+++ lessons/04_handle_get_post/handle_post.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const courses = [
+    {id: 1, name: 'Biology'},
+    {id: 2, name: 'Physics'},
+    {id: 3, name: 'Nepali'},
+]
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+// handling post request
+
+// send a course object in the body of request
+// it is json object so need to parse json by using express.json() middleware
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    } 
+    courses.push(course) //array.push appends new elements in array
+
+    res.send(course) //respond with newly posted element
+
+})
+
+app.listen(3000, console.log('listening on port 3000'))
